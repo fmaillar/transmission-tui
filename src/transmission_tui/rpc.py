74973b5cc23ac909d5d0f01970a6a28a0e690afa
Florian MAILLARD <florian.maillard.thebest@gmail.com>
2026-09-02 13:50:43 +0200

    Handle nullable Transmission RPC numeric fields

--- src/transmission_tui/rpc.py
+++ src/transmission_tui/rpc.py
@@ -79,18 +79,17 @@ class TransmissionClient:
         torrents = self._client.get_torrents(arguments=fields)
         snapshots: list[TorrentSnapshot] = []
         for torrent in torrents:
-            status = str(torrent.status)
             snapshots.append(
                 TorrentSnapshot(
-                    id=int(torrent.id),
+                    id=_int(torrent.id),
                     name=str(torrent.name),
-                    progress=float(torrent.progress),
-                    size=int(_attr(torrent, "total_size", "totalSize", default=0)),
-                    uploaded=int(_attr(torrent, "uploaded_ever", "uploadedEver", default=0)),
-                    rate_down=int(_attr(torrent, "rate_download", "rateDownload", default=0)),
-                    rate_up=int(_attr(torrent, "rate_upload", "rateUpload", default=0)),
-                    ratio=float(torrent.ratio),
-                    status=status,
+                    progress=_float(torrent.progress),
+                    size=_int(_attr(torrent, "total_size", "totalSize", default=0)),
+                    uploaded=_int(_attr(torrent, "uploaded_ever", "uploadedEver", default=0)),
+                    rate_down=_int(_attr(torrent, "rate_download", "rateDownload", default=0)),
+                    rate_up=_int(_attr(torrent, "rate_upload", "rateUpload", default=0)),
+                    ratio=_float(torrent.ratio),
+                    status=str(torrent.status),
                 )
             )
         return snapshots
@@ -127,42 +126,66 @@ class TransmissionClient:
         )
         torrent = self._client.get_torrent(torrent_id, arguments=fields)
         return TorrentDetails(
-            id=int(torrent.id),
+            id=_int(torrent.id),
             name=str(torrent.name),
             status=str(torrent.status),
-            progress=float(torrent.progress),
-            total_size=int(_attr(torrent, "total_size", "totalSize", default=0)),
-            size_when_done=int(_attr(torrent, "size_when_done", "sizeWhenDone", default=0)),
-            have_valid=int(_attr(torrent, "have_valid", "haveValid", default=0)),
-            downloaded=int(_attr(torrent, "downloaded_ever", "downloadedEver", default=0)),
-            uploaded=int(_attr(torrent, "uploaded_ever", "uploadedEver", default=0)),
-            rate_down=int(_attr(torrent, "rate_download", "rateDownload", default=0)),
-            rate_up=int(_attr(torrent, "rate_upload", "rateUpload", default=0)),
-            ratio=float(torrent.ratio),
-            eta=int(_attr(torrent, "eta", default=-1)),
-            peers_connected=int(_attr(torrent, "peers_connected", "peersConnected", default=0)),
-            peers_downloading=int(
+            progress=_float(torrent.progress),
+            total_size=_int(_attr(torrent, "total_size", "totalSize", default=0)),
+            size_when_done=_int(_attr(torrent, "size_when_done", "sizeWhenDone", default=0)),
+            have_valid=_int(_attr(torrent, "have_valid", "haveValid", default=0)),
+            downloaded=_int(_attr(torrent, "downloaded_ever", "downloadedEver", default=0)),
+            uploaded=_int(_attr(torrent, "uploaded_ever", "uploadedEver", default=0)),
+            rate_down=_int(_attr(torrent, "rate_download", "rateDownload", default=0)),
+            rate_up=_int(_attr(torrent, "rate_upload", "rateUpload", default=0)),
+            ratio=_float(torrent.ratio),
+            eta=_int(_attr(torrent, "eta", default=-1), default=-1),
+            peers_connected=_int(_attr(torrent, "peers_connected", "peersConnected", default=0)),
+            peers_downloading=_int(
                 _attr(torrent, "peers_getting_from_us", "peersGettingFromUs", default=0)
             ),
-            peers_uploading=int(
+            peers_uploading=_int(
                 _attr(torrent, "peers_sending_to_us", "peersSendingToUs", default=0)
             ),
-            webseeds_sending=int(
+            webseeds_sending=_int(
                 _attr(torrent, "webseeds_sending_to_us", "webseedsSendingToUs", default=0)
             ),
-            download_dir=str(_attr(torrent, "download_dir", "downloadDir", default="")),
-            hash_string=str(_attr(torrent, "hash_string", "hashString", default="")),
+            download_dir=_str(_attr(torrent, "download_dir", "downloadDir", default="")),
+            hash_string=_str(_attr(torrent, "hash_string", "hashString", default="")),
             added_date=_date(_attr(torrent, "added_date", "addedDate", default=0)),
             done_date=_date(_attr(torrent, "done_date", "doneDate", default=0)),
             start_date=_date(_attr(torrent, "start_date", "startDate", default=0)),
             activity_date=_date(_attr(torrent, "activity_date", "activityDate", default=0)),
-            comment=str(_attr(torrent, "comment", default="")),
-            creator=str(_attr(torrent, "creator", default="")),
-            magnet_link=str(_attr(torrent, "magnet_link", "magnetLink", default="")),
+            comment=_str(_attr(torrent, "comment", default="")),
+            creator=_str(_attr(torrent, "creator", default="")),
+            magnet_link=_str(_attr(torrent, "magnet_link", "magnetLink", default="")),
         )
 
 
+def _int(value: object, *, default: int = 0) -> int:
+    if value is None:
+        return default
+    try:
+        return int(value)
+    except (TypeError, ValueError):
+        return default
+
+
+def _float(value: object, *, default: float = 0.0) -> float:
+    if value is None:
+        return default
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        return default
+
+
+def _str(value: object) -> str:
+    return "" if value is None else str(value)
+
+
 def _date(value: object) -> str:
+    if value is None:
+        return "-"
     if isinstance(value, datetime):
         return value.astimezone().strftime("%Y-%m-%d %H:%M:%S %Z")
     try:
